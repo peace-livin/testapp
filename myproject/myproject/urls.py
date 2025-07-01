a57aad36c06fc9e4383cfbf98fbd45cc263079c7
Akshay Darade <akshaydarade6675@gmail.com>
2025-07-01 09:43:12 +0000

    add success button from bootstrap

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-
+from testapp import views
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",views.,
+    path("",views.student_view),
 ]
